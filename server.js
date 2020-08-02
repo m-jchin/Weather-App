2dd78a470ea1eec36a31c514f46ca163b7d21785
Ming <50083423+m-jchin@users.noreply.github.com>
2020-08-02 18:40:37 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,89 @@
+let http = require('http')
+
+console.log('Starting server')
+
+var cities = []; 
+let city;
+var returnLocations;
+let startup = false;
+let counter = 0;
+
+
+http.createServer((req,res) =>{
+    if (startup == false && req.method == 'GET' && req.url == '/locations')
+    {
+        res.writeHead(200, {'Content-Type' : 'application/json', 'Access-Control-Allow-Origin' : '*'})
+        let sendEmpty = JSON.stringify(cities);
+        startup = true;
+        console.log("Replying with EMPTY Array!")
+        res.write(sendEmpty)
+        res.end()
+    }
+    else if (cities.length > 0 && req.method == 'GET' && req.url == '/locations')
+    {
+        res.writeHead(200, {'Content-Type' : 'application/json', 'Access-Control-Allow-Origin' : '*'})
+        returnLocations = JSON.stringify(cities);
+        console.log('Sent to frontend: ' + returnLocations)
+        res.write(returnLocations)
+        res.end()
+    }
+    else if (startup === false && req.method == 'POST' && req.url == '/locations')
+    {
+        console.log('Startup - POST')
+        startup = true;
+
+        req.on('data', function(data) {
+            
+            city = JSON.parse(data);
+            console.log(city);
+            console.log(cities.length)
+            console.log('Startup list ready!');
+        })
+        
+        req.on('end', function() {
+            res.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin' : '*', 'Access-Control-Allow-Methods' : '*'})
+            res.end('ok')
+        })
+    }
+    else if (startup === true && req.method == 'POST' && req.url == '/locations')
+    {
+        req.on('data', function(data) {
+
+            
+            if (data === '')
+            {   
+                res.end()
+            }
+            else
+            {
+                console.log('POST')
+                console.log('Pushed to array: ' + data)
+
+                city = JSON.parse(data);
+
+                if (counter == 2){
+                    cities[counter] = city;
+                    console.log(cities)
+                    counter = 0
+                }
+                else{
+                    cities[counter] = city;
+                    counter++;
+                }
+                
+                
+            }
+        })
+        
+        req.on('end', function() {
+            res.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin' : '*', 'Access-Control-Allow-Methods' : '*'})
+            res.end('ok')
+        })
+    }
+    else
+    {
+        res.writeHead(404, {'Content-Type' : 'text/html', 'Access-Control-Allow-Origin' : '*'})
+        res.end('not found')
+    }
+}).listen(8080)
+
